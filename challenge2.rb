ff8e1b89a7b8e927f5dc029984db3146f12c7266
Filipe Sguarizi Panceri <filipe.info@gmail.com>
2015-04-16 18:12:50 -0300

    Added new challenge

--- challenge2.rb
+++ challenge2.rb
@@ -2,6 +2,6 @@ myfile = File.open(ARGV[0])
 myfile.each_line do |line|
    line.chomp!
    next if line.empty?
-   first, last = line.split(" ") 
+   first, last = line.split(" ")
    puts  "#{last} #{first}"
 end
